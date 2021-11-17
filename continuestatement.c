4e29eaa9d99e40214b7b0bc97f16fcf84c523011
vinod8833 <85753797+vinod8833@users.noreply.github.com>
2021-11-17 16:42:24 +0530

    Create continuestatement.c
    continuestatement.c

---
+++ continuestatement.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+int main()
+ {
+ 	int i;
+  for(i=1; i<=10; i++)
+  {
+  	if(i==8)
+  		continue;
+  	printf("%d ",i );
+  	  }
+  	  printf("\nlearn coding");
+  	}
+
+
+
+
+
+
+
+
+
+
+
+
