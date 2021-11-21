b58083ac258dbc6a4344239243bd57ef8c19cce0
vinod8833 <85753797+vinod8833@users.noreply.github.com>
2021-11-21 20:59:59 +0530

    Create square.c

---
+++ square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int s,a,p;
+	printf("enter the side of a square :");
+	scanf("%d",&s);
+	a=s*s;
+	p=s*4;
+	printf("area =%d ",a);	
+	printf("perimeter =%d ",p);
+	
+	return 0;	
+}
