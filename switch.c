ddd078f948b55effdaf24d088872ee6a170e6291
vinod8833 <85753797+vinod8833@users.noreply.github.com>
2021-11-21 20:55:22 +0530

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+int ch;
+	printf("enter the two numbers :");
+	scanf("%d%d",&a,&b);
+printf("enter the choice :");
+	scanf("%d",&ch);
+switch();
+{
+	case 1:c=a+b;
+	printf("%d",c);
+	break;
+	case 2:c=a-b;
+	printf("%d",c);
+	break;
+	case 3:c=a*b;
+	printf("%d",c);
+	break;
+	case 4:c=a/b;
+	printf("%d",c);
+	break;
+	case 5:c=a<b;
+	printf("%d",c);
+	break;
+	case 6:c=a>b;
+	printf("%d",c);
+	break;
+
+
+}
+}
