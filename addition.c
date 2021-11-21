58b24048f16967f16565bb77dc4e57163572e809
vinod8833 <85753797+vinod8833@users.noreply.github.com>
2021-11-21 21:21:27 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -16,3 +16,10 @@ int main()
 	int e=a+b+c+d;
 	printf("sum of numbers is : %d",e);
 }
+out put:-
+	
+Enter the frist number :76
+Enter the second number :878
+Enter the 3rd number :7867
+Enter the 4th number :989
+sum of numbers is : 9810
