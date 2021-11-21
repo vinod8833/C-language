3b8eb5e9982ee6ba8ed50b640a687ce64e9470ec
vinod8833 <85753797+vinod8833@users.noreply.github.com>
2021-11-21 21:01:45 +0530

    Create poindromenumber.c

---
+++ poindromenumber.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int rev=0,temp,n;
+	printf("enter the number :");
+	scanf("%d",&n);
+	temp=n;
+	while(temp!=0)
+	{
+		rev=rev*10;
+		rev=rev+temp%10;
+		temp=temp/10;
+			}
+printf("the reverse number %d\n",rev);
+if(n==rev)
+	printf("polindrome\n");
+else
+	printf("not polindrome\n");
+
+}
