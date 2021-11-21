b114e27c4b47ecde3a219f86a826f54c98115f7d
vinod8833 <85753797+vinod8833@users.noreply.github.com>
2021-11-21 20:43:27 +0530

    Create addfuction.c

---
+++ addfuction.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int division()
+{
+	int a,b;
+	printf("enter any two numbers: ");
+	scanf("%d%d",&a,&b);
+	return a/b;
+}
+int main()
+{
+int store=division(10,20);
+printf("%d",store);
+return 0;
+}
+	
