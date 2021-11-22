0e07a14aefb6db9aff943995a79f80c961c44d6a
vinod8833 <85753797+vinod8833@users.noreply.github.com>
2021-11-22 13:53:33 +0530

    Update switch.c

--- switch.c
+++ switch.c
@@ -1,33 +1,59 @@
 #include<stdio.h>
-void main()
+int main()
 {
-int a,b,c;
-int ch;
-	printf("enter the two numbers :");
-	scanf("%d%d",&a,&b);
-printf("enter the choice :");
-	scanf("%d",&ch);
-switch();
+char operator;
+float num1,num2,result=0;
+	printf("enter the operator (+,-,*,/) :");
+	scanf("%c",&operator);
+printf("enter the value of two operators : num1 and num2 :");
+	scanf("%f%f",&num1, &num2);
+switch(operator)
 {
-	case 1:c=a+b;
-	printf("%d",c);
+	case '+':
+	result=num1+num2;
 	break;
-	case 2:c=a-b;
-	printf("%d",c);
+	case '-':
+	result=num1-num2;
 	break;
-	case 3:c=a*b;
-	printf("%d",c);
+	case '*':
+	result=num1*num2;
 	break;
-	case 4:c=a/b;
-	printf("%d",c);
+	case '/':
+	result=num1/num2;
 	break;
-	case 5:c=a<b;
-	printf("%d",c);
-	break;
-	case 6:c=a>b;
-	printf("%d",c);
-	break;
+	
+	default:
+	printf("\n you have enetered an invalid operator ");
+}
+
+printf("the result of %.2f %c %.2f = %.2F",num1,operator,num2,result);
+return 0;
+}
 
 
-}
-}
+out put:-
+enter the operator (+,-,*,/) :=
+enter the value of two operators : num1 and num2 :45
+56
+
+ you have enetered an invalid operator the result of 45.00 = 56.00 = 0.00dileep@dileep-Latitude-E7450:~/vinod$ ./a.out 
+enter the operator (+,-,*,/) :+
+enter the value of two operators : num1 and num2 :67
+89
+the result of 67.00 + 89.00 = 156.00dileep@dileep-Latitude-E7450:~/vinod$ ./a.out 
+enter the operator (+,-,*,/) :-
+enter the value of two operators : num1 and num2 :80
+90
+the result of 80.00 - 90.00 = -10.00dileep@dileep-Latitude-E7450:~/vinod$ ./a.out 
+enter the operator (+,-,*,/) :80
+enter the value of two operators : num1 and num2 :5^[[A
+
+ you have enetered an invalid operator the result of 0.00 8 5.00 = 0.00dileep@dileep-Latitude-E7450:~/vinod$ ./a.out 
+enter the operator (+,-,*,/) :*
+enter the value of two operators : num1 and num2 :80
+5
+the result of 80.00 * 5.00 = 400.00dileep@dileep-Latitude-E7450:~/vinod$ ./a.out 
+enter the operator (+,-,*,/) :/
+enter the value of two operators : num1 and num2 :80
+5
+the result of 80.00 / 5.00 = 16.00dileep@dileep-Latitude-E7450:~/vinod$ 
