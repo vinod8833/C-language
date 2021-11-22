d8a7ef9c3e416722ea3c045ed3ae0f841b53fcd7
vinod8833 <85753797+vinod8833@users.noreply.github.com>
2021-11-22 12:56:15 +0530

    Update multiplication.c

--- multiplication.c
+++ multiplication.c
@@ -10,22 +10,14 @@
  			scanf("%d",&c);
  			printf("Enter the 4th number :");
  			scanf("%d",&d);
- 			
-
-
-
-
-
-
+ 		
 	int e=a*b*c*d;
 	printf("result :%d",e);
-
-
-
-
-
-
-
-
-
  }
+out put:-
+Enter the first number :65
+Enter the 2nd number :84
+Enter the 3rd number :93
+Enter the 4th number :88
+result :44684640dileep@dileep-Latitude-E7450:~/vinod$ 
+
